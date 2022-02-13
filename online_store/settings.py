c237d465f366fd17acb45869b8433830c37728e1
mahdi <yahoo@fake.com>
2022-02-13 10:31:19 +0330

    dev_init: initialize apps

--- online_store/settings.py
+++ online_store/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my_apps
+    'core',
+    'customer',
+    'order',
+    'product'
 ]
 
 MIDDLEWARE = [
