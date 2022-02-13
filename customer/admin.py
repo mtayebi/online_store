0d38af64eb11daed26f508dd4e25763c35803403
mahdi <yahoo@fake.com>
2022-02-13 21:54:44 +0330

    add: add customer & address admin model in customer app

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Address, Customer
+from django.utils.translation import gettext as _
+
 
 # Register your models here.
+class AddressAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('generic info', {"fields": ('province', 'city', ('alley'))}),
+        ('belongs to', {'fields': ('customer',)}),
+    )
+    list_display = [_('address_owner'), 'province', 'city', 'alley']
+    list_filter = ['province', 'city', 'alley']
+
+    def address_owner(self, obj):
+        return f'{obj.customer.first_name}-{obj.customer.last_name}'
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('customer info', {'fields': ('first_name', 'last_name', 'phone')}),
+        ('customer private info', {'fields': ('password',)}),
+    )
+    list_display = ['first_name', 'last_name', 'phone']
+
+
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Customer, CustomerAdmin)
