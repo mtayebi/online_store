7fbb677c4f1456a6966119ca122a02e4c5cbc208
mahdi <yahoo@fake.com>
2022-02-14 09:41:00 +0330

    add: add product model in product app

--- product/models.py
+++ product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from core.models import BaseModel
+from django.utils.translation import gettext as _
+
 
 # Create your models here.
+class Product(BaseModel):
+    pro_name = models.CharField(max_length=40, verbose_name=_('product_name'))
+    brand = models.CharField(max_length=30, verbose_name=_('brand'))
+    properties = models.TextField(max_length=1000, verbose_name=_('properties'))
+    stock = models.PositiveIntegerField(default=0, verbose_name=_('stock'))
+    discount = models.ForeignKey('Discount', on_delete=models.SET_DEFAULT, default=0, null=True, blank=True,
+                                 verbose_name=_('discount'))
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name=_('category'))
+
+
