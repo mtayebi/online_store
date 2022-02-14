040d9dd5359410d0076c5544f1abb444c45fc4e7
mahdi <yahoo@fake.com>
2022-02-14 10:29:49 +0330

    init: initialize admin models in admin.py product app

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from models import Product, Category, Discount
+
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class DiscountAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Discount, DiscountAdmin)
