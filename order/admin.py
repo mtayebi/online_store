53ab56e706b3bbe00c759315671e8804953fccf0
mahdi <yahoo@fake.com>
2022-02-16 09:52:00 +0330

    add: add order models into admin panel

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import OrderItem,DiscountCode, Basket
 
 # Register your models here.
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class DiscountCodeAdmin(admin.ModelAdmin):
+    pass
+
+
+class BasketAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.regidter(OrderItem, OrderItemAdmin)
+admin.site.register(DiscountCode, DiscountCodeAdmin)
+admin.site.register(Basket, BasketAdmin)
+
